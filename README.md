19be6b53eedad2910c5d5add837c32c2fd79ab44
Lucas <andre.rlucas@outlook.com>
2018-01-19 13:21:38 -0600

    Adding proyect
    Enjoy! :)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Proyect-SnakeClasic
+
+As you probably see, this game does not have score, and this is the reason why it is called Infinite Snake (haha) and I'm just thinking to add some scores, but will be in some future :) enjoy.
+<br>
+In order to launch it, just open the index.html file and you're done.
+<br>
+<br>
+<br>
+Any comments, just let me know :)
---
+++ js/script.js
@@ -0,0 +1,15 @@
+function easy() {
+    window.open('views/snake-easy.html', '_blank', "width=800,height=600");
+}
+
+function normal() {
+    window.open('views/snake-normal.html', '_blank', "width=800,height=600");
+}
+
+function hard() {
+    window.open('views/snake-hard.html', '_blank', "width=800,height=600");
+}
+
+function main() {
+    window.close();
+}
